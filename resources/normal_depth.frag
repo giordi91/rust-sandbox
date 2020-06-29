b31324cbce979a0c3e204b3ffee5ebf4267890cf
Marco Giordano <marco.giordano.work@gmail.com>
2020-06-29 21:17:16 +0100

    cleaning up

--- resources/normal_depth.frag
+++ resources/normal_depth.frag
@@ -3,26 +3,8 @@
 layout(location=0) in vec2 i_uv;
 layout(location=0) out vec4 f_color;
 
-layout(set=0, binding=0)
-uniform Uniforms {
-    mat4 u_view_proj; 
-    mat4 u_view_proj_inverse; 
-};
-
-
-
 layout(set = 1, binding = 0) uniform texture2D  in_tex;
 layout(set = 1, binding = 1) uniform sampler in_tex_sampler;
-
-
-vec3 reconstructPosition(in vec2  uv, in float z, in mat4 InvVP)
-{
-  float x = uv.x * 2.0f - 1.0f;
-  float y = (1.0 - uv.y) * 2.0f - 1.0f;
-  vec4 position_s = vec4(x, y, z, 1.0f);
-  vec4 position_v = InvVP* position_s;
-  return position_v.xyz / position_v.w;
-}
 
 void main() {
 
--- src/sandbox.rs
+++ src/sandbox.rs
@@ -19,7 +19,6 @@ unsafe impl bytemuck::Zeroable for ObjectBuffer {}
 
 pub struct Sandbox {
     engine_runtime: platform::EngineRuntime,
-    render_pipeline_handle: handle::ResourceHandle,
     depth_only_pipeline: handle::ResourceHandle,
     normal_pipeline: handle::ResourceHandle,
     normal_compute_pipeline: handle::ResourceHandle,
@@ -30,7 +29,6 @@ pub struct Sandbox {
     normal_compute_bgs: Vec<wgpu::BindGroup>,
     per_object_bind_groups: Vec<wgpu::BindGroup>,
     size: winit::dpi::PhysicalSize<u32>,
-    color: f64,
     camera_controller: graphics::camera::CameraControllerFPS,
     per_frame_data: graphics::FrameData,
     time_stamp: u64,
@@ -46,7 +44,6 @@ impl platform::Application for Sandbox {
     async fn new(window: &Window, mut engine_runtime: platform::EngineRuntime) -> Self {
         let size = window.inner_size();
 
-        let color = 0.0;
         let gpu_interfaces = &engine_runtime.gpu_interfaces;
 
         let camera = graphics::camera::Camera {
@@ -92,17 +89,6 @@ impl platform::Application for Sandbox {
             .await;
 
         let default_depth_format = wgpu::TextureFormat::Depth32Float;
-
-        let render_pipeline_handle = engine_runtime
-            .resource_managers
-            .pipeline_manager
-            .load_pipeline(
-                "resources/gltf_model.pipeline",
-                &mut engine_runtime.resource_managers.shader_manager,
-                &engine_runtime.gpu_interfaces,
-                default_depth_format,
-            )
-            .await;
 
         let normal_compute_pipeline= engine_runtime
             .resource_managers
@@ -297,7 +283,6 @@ impl platform::Application for Sandbox {
             bytemuck::cast_slice(&matrices[..]),
             wgpu::BufferUsage::UNIFORM | wgpu::BufferUsage::COPY_DST,
         );
-        let matrices_size = matrices.len() * std::mem::size_of::<ObjectBuffer>();
         let matrix_size = std::mem::size_of::<ObjectBuffer>();
 
         //the second layout is the one on a per object basis, so we create one per
@@ -331,7 +316,6 @@ impl platform::Application for Sandbox {
 
         Self {
             engine_runtime,
-            render_pipeline_handle,
             depth_only_pipeline,
             normal_pipeline,
             normal_compute_pipeline,
@@ -342,7 +326,6 @@ impl platform::Application for Sandbox {
             normal_compute_bgs,
             per_object_bind_groups,
             size,
-            color,
             camera_controller,
             per_frame_data,
             time_stamp: platform::core::get_time_in_micro(),
@@ -432,26 +415,9 @@ impl platform::Application for Sandbox {
             });
             let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 color_attachments: &[],
-                //color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
-                //    attachment: &frame.view,
-                //    resolve_target: None,
-                //    load_op: wgpu::LoadOp::Clear,
-                //    store_op: wgpu::StoreOp::Store,
-                //    clear_color: wgpu::Color {
-                //        r: 0.1,
-                //        g: 0.2,
-                //        b: self.color,
-                //        a: 1.0,
-                //    },
-                //}],
                 depth_stencil_attachment,
             });
 
-            //let render_pipeline = self
-            //    .engine_runtime
-            //    .resource_managers
-            //    .pipeline_manager
-            //    .get_pipeline_from_handle(&self.render_pipeline_handle);
             let render_pipeline = self
                 .engine_runtime
                 .resource_managers
@@ -475,15 +441,6 @@ impl platform::Application for Sandbox {
                         .resource_managers
                         .buffer_manager
                         .get_buffer_from_handle(&pos_idx);
-
-                    //let n_mapper =
-                    //    mesh.get_buffer_from_semantic(graphics::model::MeshBufferSemantic::Normals);
-                    //let n_idx = n_mapper.buffer_idx;
-                    //let n_buff = self
-                    //    .engine_runtime
-                    //    .resource_managers
-                    //    .buffer_manager
-                    //    .get_buffer_from_handle(&n_idx);
 
                     let mut idx_count = 0;
                     match &mesh.index_buffer {
@@ -534,10 +491,10 @@ impl platform::Application for Sandbox {
                     load_op: wgpu::LoadOp::Clear,
                     store_op: wgpu::StoreOp::Store,
                     clear_color: wgpu::Color {
-                        r: 0.1,
-                        g: 0.2,
-                        b: self.color,
-                        a: 1.0,
+                        r: 0.0,
+                        g: 0.0,
+                        b: 0.0,
+                        a: 0.0,
                     },
                 }],
                 depth_stencil_attachment: None,
@@ -558,11 +515,6 @@ impl platform::Application for Sandbox {
             render_pass.set_bind_group(0, &self.normal_bgs.get(0).unwrap(), &[]);
             render_pass.set_bind_group(1, &self.normal_bgs.get(1).unwrap(), &[]);
             render_pass.draw(0..6, 0..1);
-        }
-
-        self.color += 0.001;
-        if self.color > 1.0 {
-            self.color = 0.0;
         }
 
         command_buffers.push(encoder.finish());
